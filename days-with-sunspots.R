ddb5d6f67abb314a6fc8ed5a5d749a2c69c11136
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2019-11-06 18:34:03 -0500

    Added code to calculate monthly means
    Calculate mean days per month with spots.

--- days-with-sunspots.R
+++ days-with-sunspots.R
@@ -2,28 +2,6 @@ library(tidyverse)
 library(data.table)
 library(xts)
 rm(list=ls())
-## Basic Plot
-# ggplot(data=NOAA,aes(x=Ymd,y=Ra)) + geom_col()
-# A <- as.data.frame(lowess(NOAA$Ra,f=.2))
-# B <-cbind(NOAA,A)
-# ###
-# ggplot(data=B,aes(x=Ymd,y=Ra,col="Ra")) + geom_line() +
-#   geom_line(data=B,aes(x=Ymd,y=y,col="Y"))
-#   B$Vote <- ifelse(B$Ra ==0,0,1)
-# B <- B %>% filter(B$Vote==1)
-# #
-# # Create monthly summary field with XTS
-# isn.xts <- xts(x = NOAA$Vote, order.by = NOAA$Ymd)
-# isn.monthly <- apply.monthly(isn.xts, sum)
-# isn <-as.data.table(isn.monthly)
-# D <- NOAA %>% filter(Ymd >="2005-01-01" & Ymd <="2014-01-01" )
-# ggplot(data=D,aes(x=Ymd,y=Ra)) +geom_line() +geom_smooth(method="loess")
-# 
-# isn.xts <- xts(x = NOAA$Vote, order.by = NOAA$Ymd)
-# isn.monthly <- apply.monthly(isn.xts, sum)
-# isn <-as.data.table(isn.monthly)
-# D <- NOAA %>% filter(Ymd >="2005-01-01" & Ymd <="2014-01-01" )
-# ggplot(data=D,aes(x=Ymd,y=Ra)) +geom_line() +geom_smooth(method="loess")
 ##
 ## Begin SIDC daily numbers stuff
 ##
@@ -31,18 +9,27 @@ sidc <-fread("http://sidc.be/silso/DATA/SN_d_tot_V2.0.csv",sep = ';')
 colnames(sidc) <- c("Year","Month","Day", "Fdate","Spots", "Sd","Obs" ,"Defin"  )
 sidc$Ymd <- as.Date(paste(sidc$Year, sidc$Month, sidc$Day, sep = "-"))
 sidc1<-sidc[Ymd>="1850-01-01",.(Ymd,Spots),]
-sidc1$Vote <- ifelse(sidc$Spots ==0,0,1)
-Fit <- as.data.frame(lowess(sidc1$Spots,f=.2))
+sidc1$Vote <- ifelse(sidc1$Spots ==0,0,1)
+Fit <- as.data.frame(lowess(sidc1$Spots,f=0.3))
 sidc1 <-cbind(sidc1,Fit$y)
-colnames(sidc1) <-c("Ymd","Spots","Loess")
+colnames(sidc1) <-c("Ymd","Spots","Vote","Loess")
+##
+## Quick PLot of  Mens and Leoss for 1850 to 2019
+ggplot(data=sidc1,aes(x=Ymd,y=Spots,col="Sidc")) + geom_line() +
+  geom_line(data=sidc1,aes(x=Ymd,y=Loess,col="Loess")) + ggtitle("SIDC Daily Mean: 1850 - 2019")
 ##
 ## Current Solar Miniumn: 2014 - Oct. 2019
 ##
-sidc2 <-  sidc2 &>% filter(Ymd >="2014-01-01") 
-ggplot(data=sidc2,aes(x=Ymd,y=Spots)) + geom_line() + geom_line(data=sdic2,aes(x=Ymd,y=Loess))
+sidc2 <-  sidc1 %>% filter(Ymd >="2014-01-01") 
+ggplot(data=sidc2,aes(x=Ymd,y=Spots,col="Sidc")) + geom_line() + 
+  geom_line(data=sidc2,aes(x=Ymd,y=Loess,col="Loess")) + ggtitle("Current Solar Min. 2014-2019")
+##
 ## Create monthly summary field with XTS
+##
 isn.xts <- xts(x = sidc1$Vote, order.by = sidc1$Ymd)
 isn.monthly <- apply.monthly(isn.xts, sum)
 isn <-as.data.table(isn.monthly)
-D <- sidc1 %>% filter(Ymd >="2014-01-01")
-ggplot(data=D,aes(x=Ymd,y=Ra)) +geom_line() +geom_smooth(method="loess")
+colnames(isn) <- c("Ymd","Days")
+D <- isn %>% filter(Ymd >="2014-01-01")
+ggplot(data=D,aes(x=Ymd,y=Days)) +geom_line() +geom_smooth(method="loess",col="blue") +
+  ggtitle("Monthly Mean Days with Sunspots: 2014-2019")
